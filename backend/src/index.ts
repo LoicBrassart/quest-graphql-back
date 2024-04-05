1204fec972e439bea6fc75321b4461fa68334535
LoicBrassart <loic.brassart@gmail.com>
2024-04-05 10:41:01 +0200

    Add basic GrapQL server

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,49 @@
 import express from "express";
-const port = 5000;
-const app = express();
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+const typeDefs = `#graphql
+  type Book {
+    title:String
+    author:String
+  }
+
+  type Query {
+    books: [Book]
+  }
+`;
+
+const books = [
+  {
+    title: "The Awakening",
+    author: "K.Chopin",
+  },
+  {
+    title: "The Awakening",
+    author: "K.Chopin",
+  },
+];
+
+const resolvers = {
+  Query: {
+    books: () => {
+      return books;
+    },
+    // books: () => books
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs: typeDefs,
+  // typeDefs
+  resolvers: resolvers,
+  // resolvers
 });
 
-app.listen(port, async () => {
-  console.log(`project's backend listening on http://localhost:${port}`);
-});
+const startApollo = async () => {
+  const { url } = await startStandaloneServer(server, {
+    listen: { port: 5000 },
+  });
+  console.log(`🚀  Server ready at: ${url}`);
+};
+startApollo();
